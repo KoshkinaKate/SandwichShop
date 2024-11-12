0ac77feb75c666db2f9d96a65a7bb485f18d2303
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-12 09:23:20 -0500

    Completed Chips class

--- src/main/java/com/FreshFit/items/Chips.java
+++ src/main/java/com/FreshFit/items/Chips.java
@@ -3,10 +3,19 @@ package com.FreshFit.items;
 import com.FreshFit.utilities.IPrice;
 
 public class Chips implements IPrice {
+    private String type; //BBQ, Spicy etc
+    private final double price = 1.50;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
     @Override
     public double getPrice() {
-        return 0;
+        return price;
     }
 }
-//Represents a chips item, implements iPrice
-//String type
--- src/main/java/com/FreshFit/orders/Order.java
+++ src/main/java/com/FreshFit/orders/Order.java
@@ -1,6 +1,7 @@
 package com.FreshFit.orders;
 
 public class Order {
+
 }
 // Keeps track of items in the order and calculates the total
 //List<Sandwich> same with drink, chips.
