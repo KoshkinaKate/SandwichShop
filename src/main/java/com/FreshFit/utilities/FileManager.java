d8bf86371a2f0311b75f9d2ddbea35c544ab6f60
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-12 14:41:49 -0500

    Completed FileManager

--- src/main/java/com/FreshFit/utilities/FileManager.java
+++ src/main/java/com/FreshFit/utilities/FileManager.java
@@ -1,6 +1,21 @@
 package com.FreshFit.utilities;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class FileManager {
+    //static - method can be called directly by using a class name. No need to create an Object
+    public static void generateReceipt(String orderDetails){
+        String fileName = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(orderDetails);
+            System.out.println("Thank you for the purchase, your receipt " + fileName + " was saved.");
+        } catch (IOException e) {
+            System.err.println("Error saving receipt: " + e.getMessage());
+        }
+    }
 }
-//here is the buffered writer
-//only one method which is generateReceipt
