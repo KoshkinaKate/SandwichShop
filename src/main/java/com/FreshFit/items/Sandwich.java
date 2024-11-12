ffb81f7e471cbe07f2da3f72c20d4ba7e45208c5
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-12 10:09:07 -0500

    Updated Sandwich class

--- src/main/java/com/FreshFit/items/Sandwich.java
+++ src/main/java/com/FreshFit/items/Sandwich.java
@@ -1,13 +1,45 @@
 package com.FreshFit.items;
 
+import com.FreshFit.components.Bread;
+import com.FreshFit.components.Topping;
 import com.FreshFit.utilities.IPrice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich implements IPrice {
+    private String size;
+    private boolean toasted;
+    private Bread bread;
+    private List<Topping> toppings;
+
+    public Sandwich(String size, boolean toasted, Bread bread) {
+        this.size = size;
+        this.toasted = toasted;
+        this.bread = bread;
+        this.toppings = new ArrayList<>();
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public Bread getBread() {
+        return bread;
+    }
+
+    //premium and regular toppings
+    public void addTopping(Topping topping){
+        toppings.add(topping);
+    }
+
+
     @Override
     public double getPrice() {
-        return 0;
+        double totalPrice = 
     }
 }
-//Represents a sandwich, implements iPrice
-// String size, List<Toppings>, boolean toasted, Bread bread
-//addTopping() void
