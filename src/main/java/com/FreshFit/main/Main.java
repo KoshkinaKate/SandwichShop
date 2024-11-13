c7201b328d5024e194cff7fb79738ea3b40c3762
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-13 09:17:44 -0500

    Partially completed HomeScreen class

--- src/main/java/com/FreshFit/main/Main.java
+++ src/main/java/com/FreshFit/main/Main.java
@@ -3,6 +3,6 @@ package com.FreshFit.main;
 public class Main {
     public static void main(String[] args) {
 //        UserInterface ui = new UserInterface();
-//        ui.display();
+//        ui.displayHomeScreen();
     }
 }
--- src/main/java/com/FreshFit/ui/HomeScreen.java
+++ src/main/java/com/FreshFit/ui/HomeScreen.java
@@ -1,7 +1,23 @@
 package com.FreshFit.ui;
 
+import java.util.Scanner;
+
 public class HomeScreen {
+   private Scanner scanner = new Scanner(System.in);
+   private UserInterface ui;
+
+    public HomeScreen(UserInterface ui) {
+        this.ui = ui;
+    }
+
+    public void displayMenu(){
+        while(true){
+            System.out.println("Welcome to Fresh and Fit! ");
+            System.out.println("1. New Order");
+            System.out.println("2. Exit");
+
+            System.out.println("Please choose an option: ");
+            int option = scanner.nextInt();
+        }
+    }
 }
-//has while loop and pass to other screen the order that it has been working on
-// all screens need to operate on the same Order instance ->should accept the Order instance as a parameter, through the constructor
-// 2 Options - New Order and Exit
