6e0f93c7bf5759543cd218bf9c9d188b93986327
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-14 12:11:54 -0500

    Set Up for Order Test Unit

---
+++ src/test/java/com/FreshFit/orders/OrderTest.java
@@ -0,0 +1,15 @@
+package com.FreshFit.orders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+    @Test
+    public void getTotalPrice_should_calculate_SandwichDrinkChipsAmount(){
+        //arrange
+        //act
+        //assert
+    }
+
+}
