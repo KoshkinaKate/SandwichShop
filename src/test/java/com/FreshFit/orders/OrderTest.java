284c6d36792f2641de2176fc4378389f9187b179
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-14 14:45:57 -0500

    Order Test Unite Completed

--- src/test/java/com/FreshFit/orders/OrderTest.java
+++ src/test/java/com/FreshFit/orders/OrderTest.java
@@ -1,6 +1,14 @@
 package com.FreshFit.orders;
 
+import com.FreshFit.components.Bread;
+import com.FreshFit.components.Cheese;
+import com.FreshFit.components.Meat;
+import com.FreshFit.items.Chips;
+import com.FreshFit.items.Drink;
+import com.FreshFit.items.Sandwich;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,8 +16,26 @@ class OrderTest {
     @Test
     public void getTotalPrice_should_calculate_SandwichDrinkChipsAmount(){
         //arrange
+        Sandwich sandwich = new Sandwich("4\"",true,new Bread("white"));
+        sandwich.addToppings(List.of(new Meat("Turkey"), new Cheese("Swiss")));
+
+        Drink drink = new Drink("Medium", "Water");
+
+        Chips chip = new Chips("BBQ");
+
+        Order currentOrder = new Order();
+
+        currentOrder.addSandwich(sandwich);
+        currentOrder.addDrink(drink);
+        currentOrder.addChips(chip);
+
+        double expectedTotalPrice = sandwich.getPrice() + drink.getPrice() + chip.getPrice();
+
         //act
+        double actualTotalPrice = currentOrder.getTotalPrice();
+
         //assert
+        assertEquals(expectedTotalPrice,actualTotalPrice);
     }
 
 }
